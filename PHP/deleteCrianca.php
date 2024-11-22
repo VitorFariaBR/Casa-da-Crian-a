a892b7246525b71b17967bd2b663bdc83340d103
Vitor Faria <vitorlucas123faria@gmail.com>
2024-11-21 21:54:05 -0300

    Update deleteCrianca.php

--- PHP/deleteCrianca.php
+++ PHP/deleteCrianca.php
@@ -1,30 +1,30 @@
 <?php 
-    session_start();
+session_start();
 
-    if(!isset($_SESSION) || ($_SESSION["loggedin"] == false)){
-        header("Location: painel.php");
-    }
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
+    header("Location: painel.php");
+    exit;
+}
 
 include 'conect.php';  
 
-if(isset($_GET['ID_ALUNO'])){
+if (isset($_GET['ID_ALUNO'])) {
     $idCrianca = $_GET['ID_ALUNO'];
     $con = conect::conectar();
-    try{
+    try {
         $stmt = $con->prepare('DELETE FROM aluno WHERE ID_ALUNO = :v1');
-
-        $stmt->execute(array(
-            ':v1' => $idCrianca
-        ));
+        $stmt->execute([':v1' => $idCrianca]);
         
-        if($stmt->rowCount() > 0){
-            header("Location: listCrianca.php"); 
-        }        
-    }catch(PDOException $e){
+        if ($stmt->rowCount() > 0) {
+            header("Location: listCrianca.php");
+            exit;
+        } else {
+            echo "Nenhum registro foi excluído.";
+        }
+    } catch (PDOException $e) {
         echo 'Erro: ' . $e->getMessage();
-    }  
-}else{
+    }
+} else {
     echo "Criança não identificada";
 }
-
 ?>
